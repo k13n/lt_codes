8ab3d7f2311006d1986c9a3841a86c53632a3f64
Kevin Wellenzohn <kevin.wellenzohn@gmail.com>
2013-12-30 19:51:35 +0100

    Initial commit, set up directory structure

---
+++ src/main/java/com/k13n/lt_codes/App.java
@@ -0,0 +1,13 @@
+package com.k13n.lt_codes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
