c9771c0ffc306b773092c756ab0da9d25618e28a
Kevin Wellenzohn <kevin.wellenzohn@gmail.com>
2014-01-01 14:12:54 +0100

    Create skeleton of the App class

--- src/main/java/com/k13n/lt_codes/App.java
+++ src/main/java/com/k13n/lt_codes/App.java
@@ -1,13 +1,34 @@
 package com.k13n.lt_codes;
 
-/**
- * Hello world!
- *
- */
-public class App
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.io.File;
+
+public class App {
+  private final File file;
+
+  public App(String[] args) {
+    String filename = parseFilename(args);
+    file = ensureFileExists(filename);
+  }
+
+  private String parseFilename(String[] args) {
+    if (args.length == 0)
+      throw new IllegalArgumentException("No file provided");
+    return args[0];
+  }
+
+  private File ensureFileExists(String filename) {
+    File file = new File(filename);
+    if (!file.exists())
+      throw new IllegalArgumentException("File does not exist");
+    return file;
+  }
+
+  public void execute() {
+    // FIXME
+  }
+
+  public static void main(String[] args) {
+    new App(args).execute();
+  }
+
 }
