f5baf5546a77a3bc2b85418ee51a6d6c52fc841e
Kevin Wellenzohn <kevin.wellenzohn@gmail.com>
2014-01-02 15:44:38 +0100

    Use some binary data (image) in AppTest

--- src/test/java/com/k13n/lt_codes/AppTest.java
+++ src/test/java/com/k13n/lt_codes/AppTest.java
@@ -9,13 +9,14 @@ import static org.junit.Assert.*;
 public class AppTest {
   private static final String FILENAME = "firework.jpg";
   private static final String OUTFILE = "/tmp/" + FILENAME + ".out";
-  private static final int PACKET_SIZE = 512;
+  private static final int PACKET_SIZE = 1024;
 
   @Test
   public void itWorksWithPerfectChannel() throws Exception {
     byte[] data = readFile(FILENAME);
     Encoder enc = new Encoder(data, PACKET_SIZE);
-    final Decoder dec = new DefaultDecoder(enc.getSeed(), enc.getNPackets());
+//    final Decoder dec = new DefaultDecoder(enc.getSeed(), enc.getNPackets());
+    final Decoder dec = new IncrementalDecoder(enc.getNPackets(), PACKET_SIZE);
 
     enc.encode(new Encoder.Callback() {
       public boolean call(Encoder encoder, TransmissonPacket packet) {
@@ -23,18 +24,19 @@ public class AppTest {
       }
     });
 
+    dec.write(new FileOutputStream(OUTFILE));
+
     ByteArrayOutputStream bos = new ByteArrayOutputStream();
     dec.write(bos);
     assertArrayEquals(data, bos.toByteArray());
-
-    dec.write(new FileOutputStream(OUTFILE));
   }
 
   @Test
   public void itWorksWithLossyChannel() throws Exception {
     byte[] data = readFile(FILENAME);
     Encoder enc = new Encoder(data, PACKET_SIZE);
-    final Decoder dec = new DefaultDecoder(enc.getSeed(), enc.getNPackets());
+//    final Decoder dec = new DefaultDecoder(enc.getSeed(), enc.getNPackets());
+    final Decoder dec = new IncrementalDecoder(enc.getNPackets(), PACKET_SIZE);
 
     final ErasureChannel channel = new ErasureChannel(
       new ErasureChannel.Callback() {
@@ -50,11 +52,11 @@ public class AppTest {
       }
     });
 
+    dec.write(new FileOutputStream(OUTFILE));
+
     ByteArrayOutputStream bos = new ByteArrayOutputStream();
     dec.write(bos);
     assertArrayEquals(data, bos.toByteArray());
-
-    dec.write(new FileOutputStream(OUTFILE));
   }
 
   private byte[] readFile(String filename) {
