a714b0298f5d6a86c41a38a3cc2aac9bd5f1f06e
Kevin Wellenzohn <kevin.wellenzohn@gmail.com>
2014-01-03 14:02:27 +0100

    Simplify the Client class further

--- src/main/java/com/k13n/lt_codes/Client.java
+++ src/main/java/com/k13n/lt_codes/Client.java
@@ -1,15 +1,13 @@
 package com.k13n.lt_codes;
 
 import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 public class Client {
   private final Decoder decoder;
   private ExecutorService executor;
-  private Future<Boolean> lastJobFuture;
 
   public Client() {
     executor = Executors.newSingleThreadExecutor();
@@ -18,11 +16,10 @@ public class Client {
 
   public synchronized void stopProcessing() {
     try {
-      lastJobFuture.get();
-    } catch (InterruptedException | ExecutionException e) {
+      executor.shutdown();
+      executor.awaitTermination(Long.MAX_VALUE, TimeUnit.SECONDS);
+    } catch (InterruptedException e) {
       e.printStackTrace();
-    } finally {
-      executor.shutdownNow();
     }
   }
 
@@ -30,7 +27,7 @@ public class Client {
     if (decoder.isDecodingFinished())
       return;
 
-    lastJobFuture = executor.submit(new Callable<Boolean>() {
+    executor.submit(new Callable<Boolean>() {
       @Override public Boolean call() throws Exception {
         return decoder.receive(packet);
       }
