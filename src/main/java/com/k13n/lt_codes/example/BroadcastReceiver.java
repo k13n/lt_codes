b38c90af38325502f0b5f015ed71ff3d5af2cd24
Kevin Wellenzohn <kevin.wellenzohn@gmail.com>
2014-01-10 09:14:55 +0100

    Add BroadcastReceiver and BroadcastSender classes

---
+++ src/main/java/com/k13n/lt_codes/example/BroadcastReceiver.java
@@ -0,0 +1,75 @@
+package com.k13n.lt_codes.example;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.UnknownHostException;
+import com.k13n.lt_codes.DecodedPacket;
+import com.k13n.lt_codes.Decoder;
+import com.k13n.lt_codes.EncodedPacket;
+import com.k13n.lt_codes.IncrementalDecoder;
+
+public class BroadcastReceiver {
+  private final MulticastSocket socket;
+  private final InetAddress group;
+  private final Decoder decoder;
+  private final byte[] buffer;
+
+  public BroadcastReceiver() throws IOException {
+    socket = new MulticastSocket(BroadcastSender.OUTBOUND_PORT);
+    group = resolveGroup();
+    socket.joinGroup(group);
+    decoder = new IncrementalDecoder(Server.DEFAULT_PACKET_SIZE);
+    buffer = new byte[EncodedPacket.MAX_PACKET_SIZE];
+  }
+
+  private InetAddress resolveGroup() throws UnknownHostException {
+    return InetAddress.getByName(BroadcastSender.BROADCAST_ADDRESS);
+  }
+
+  public void receive() {
+    while (!decoder.isDecodingFinished()) {
+      DatagramPacket datagram = new DatagramPacket(buffer, buffer.length);
+      waitForDatagram(datagram);
+    }
+  }
+
+  private void waitForDatagram(DatagramPacket datagram) {
+    try {
+      socket.receive(datagram);
+      EncodedPacket encodedPacket = new EncodedPacket(datagram.getData());
+      DecodedPacket packet = encodedPacket.decode();
+      decoder.receive(packet);
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void write(String filename) {
+    try {
+      FileOutputStream fos = new FileOutputStream(new File(filename));
+      decoder.write(fos);
+      fos.flush();
+      fos.close();
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    System.out.println("receiver: starting up");
+    BroadcastReceiver receiver = new BroadcastReceiver();
+    receiver.receive();
+    receiver.write("");
+    System.out.println("receiver: shutting down");
+  }
+
+}
---
+++ src/main/java/com/k13n/lt_codes/example/BroadcastSender.java
@@ -0,0 +1,66 @@
+package com.k13n.lt_codes.example;
+
+import java.io.File;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+import com.k13n.lt_codes.DecodedPacket;
+import com.k13n.lt_codes.EncodedPacket;
+import com.k13n.lt_codes.ErasureChannel;
+import com.k13n.lt_codes.ErasureChannel.Callback;
+
+public class BroadcastSender {
+  public static final int SOCKET_PORT = 4445;
+  public static final int OUTBOUND_PORT = 4446;
+  public static final String BROADCAST_ADDRESS = "230.0.0.1";
+
+  private final DatagramSocket socket;
+  private final Server server;
+  private final InetAddress group;
+
+  public BroadcastSender(File file) throws SocketException,
+      UnknownHostException {
+    socket = new DatagramSocket(SOCKET_PORT);
+    group = resolveGroup();
+    server = new FountainServer(file, setUpChannel());
+  }
+
+  private InetAddress resolveGroup() throws UnknownHostException {
+    return InetAddress.getByName(BROADCAST_ADDRESS);
+  }
+
+  private ErasureChannel setUpChannel() {
+    return new ErasureChannel(0.0, new Callback() {
+      @Override
+      public void call(ErasureChannel channel, DecodedPacket packet) {
+        sendPacket(packet);
+      }
+    });
+  }
+
+  private void sendPacket(DecodedPacket sourcePacket) {
+    try {
+      byte[] data = EncodedPacket.encode(sourcePacket).toByteArray();
+      DatagramPacket datagram = new DatagramPacket(data, 0, data.length, group,
+          OUTBOUND_PORT);
+      socket.send(datagram);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void startBroadcast() {
+    server.startTransmission();
+  }
+
+  public static void main(String[] args) throws SocketException, UnknownHostException {
+    System.out.println("sender: starting up");
+    File file = new File("");
+    new BroadcastSender(file).startBroadcast();
+    System.out.println("sender: shutting down");
+  }
+
+}
